e64933507ebf27bf8a4e8ace6a0ea61b280b1e6a
loretta-dhahabu <loreydjefwa@gmail.com>
2022-08-18 22:59:27 +0300

    Done.

---
+++ app.rb
@@ -0,0 +1,14 @@
+#app.rb
+#use puts and print for string data type
+puts "Hello World!"
+
+print "Pass this test, please."
+
+#use p for arrays
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+#use pp method to output complex data such as nested arrays and hashes
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
