13b405103498173f55bf9ef76b45c07e6310ed28
loretta-dhahabu <loreydjefwa@gmail.com>
2022-08-22 15:20:24 +0300

    commit tree b590288eb13430da2f1a771849836acc5b84ea80

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ print "Pass this test, please."
 #use p for arrays
 # This is the equivalent of calling:
 # puts [1, 2, 3].inspect
-p [1, 2, 3]
+p [1,2,3]
 
 #use pp method to output complex data such as nested arrays and hashes
 #pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
